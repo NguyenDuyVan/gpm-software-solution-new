ed678ed83852ead50cab09ffb985d3012de80430
NguyenDuyVan <duyvan866@gmail.com>
2025-05-16 16:17:40 +0700

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="bg-white dark:bg-gray-800 text-right py-2 px-4 text-[10px] md:text-xs">
-    <p>© 2024 GPM Software Solutions Limited Co. All Rights Reserved.</p>
+    <p>© 2018-2025 GPM Software Solutions Limited Co. All Rights Reserved.</p>
   </div>
 </template>
